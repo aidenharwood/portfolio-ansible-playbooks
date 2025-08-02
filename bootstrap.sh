fc895ddf540e9f16be5bdfe8020fa9a288227efc
carbage <carbage@github.com>
2025-08-02 19:43:05 +0100

    Add crontab to bootstrap, tidy up file refs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,19 @@
 sudo apt update
 sudo apt install -y ansible
 
+ansibleDir="~/.portfolio-ansible-playbooks"
+ansiblePlaybook="playbooks/kubernetes.yml"
+
 # Clone playbook git repo
-if [ ! -d "~/.portfolio-ansible-playbooks" ]; then
-    git clone https://github.com/aidenharwood/portfolio-ansible-playbooks.git
+if [ ! -d $ansibleDir ]; then
+    git clone https://github.com/aidenharwood/portfolio-ansible-playbooks.git $ansibleDir
 fi
 
 # Navigate to the playbook directory
-cd ~/.portfolio-ansible-playbooks || exit
+cd $ansibleDir || exit
 
 # Run Ansible playbook
-ansible-playbook -i inventory playbooks/main.yml
+ansible-playbook -i localhost, $ansiblePlaybook
+
+$crontab = "0 0 * * * cd $ansibleDir && git pull origin main && ansible-playbook -i localhost, $ansiblePlaybook"
+#(crontab -l 2>/dev/null; echo "$crontab") | crontab -
